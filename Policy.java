abc7bfacdd5add1b59c30e986a8b19c2fa02be54
Julien Shishkovskiy <37152540+Djis247@users.noreply.github.com>
2023-02-25 11:42:05 -0500

    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -32,29 +32,47 @@ public class Policy {
    }
    
    //Accessor methods
+   /** @return the policy number */
    public int getPolicyNumber()                 { return policyNumber; }
+   /** @return the provider's name */
    public String getProviderName()              { return providerName; }
+   /** @return the policyholder's first name */
    public String getPolicyholderFirstName()     { return phFirstName; }
+   /** @return the policyholder's last name */
    public String getPolicyholderLastName()      { return phLastName; }
+   /** @return the policyholder's age */
    public int getPolicyholderAge()              { return phAge; }
+   /** @return the smoking status of the policyholder */
    public String getPolicyholderSmokingStatus() { return phSmokingStatus; }
+   /** @return the policyholder's height in inches */
    public double getPolicyholderHeight()        { return phHeight; }
+   /** @return the policyholder's weight in pounds */
    public double getPolicyholderWeight()        { return phWeight; }
    
    //Mutator methods
+   /** @param pn the policy number */
    public void setPolicyNumber(int pn)                   { this.policyNumber = pn; }
+   /** @param pn the provider's name */
    public void setProviderName(String pn)                { this.providerName = pn; }
+   /** @param fn the policyholder's first name */
    public void setPolicyholderFirstName(String fn)       { this.phFirstName = fn; }
+   /** @param ln the policyholder's last name */
    public void setPolicyholderLastName(String ln)        { this.phLastName = ln; }
+   /** @param age the policyholder's age */
    public void setPolicyholderAge(int age)               { this.phAge = age; }
+   /** @param ss the policyholder's smoking status either "smoker" or "non-smoker" */
    public void setPolicyholderSmokingStatus(String ss)   { this.phSmokingStatus = ss; }
+   /** @param h the policyholder's height in inches */
    public void setPolicyholderHeight(double h)           { this.phHeight = h; }
+   /** @param w the policyholder's weight in pounds */
    public void setPolicyholderWeight(double w)           { this.phWeight = w; }
    
    //Calculating methods
+   /** @return the BMI of the policyholder */
    public double calcBMI() {
       return (this.phWeight * 703) / (this.phHeight * this.phHeight);
    }
+   /** @return the price of the policy */
    public double calcPolicyPrice() {
       double price = 600;
       if (this.phAge > 50) price += 75;
