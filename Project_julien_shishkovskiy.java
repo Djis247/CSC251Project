b2d4b9ef464f65ab54203a99e5c2ae42ff860ff2
Julien Shishkovskiy <37152540+Djis247@users.noreply.github.com>
2023-02-25 12:20:55 -0500

    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_julien_shishkovskiy.java
+++ Project_julien_shishkovskiy.java
@@ -32,6 +32,26 @@ public class Project_julien_shishkovskiy {
             Policy policy = new Policy(policyNumber, providerName, phFirst, phLast, phAge, phSmokingStatus, phHeight, phWeight);
             policies.add(policy);
          }
+         
+         int numSmoker = 0;
+         for(Policy policy : policies) {
+            if (policy.getPolicyholderSmokingStatus().equalsIgnoreCase("smoker")) numSmoker++;
+            
+            System.out.println("");
+            System.out.printf("Policy Number: %d\n",                 policy.getPolicyNumber());
+            System.out.printf("Provider Name: %s\n",                 policy.getProviderName());
+            System.out.printf("Policyholder's First Name: %s\n",     policy.getPolicyholderFirstName());
+            System.out.printf("Policyholder's Last Name: %s\n",      policy.getPolicyholderLastName());
+            System.out.printf("Policyholder's Age: %d\n",            policy.getPolicyholderAge());
+            System.out.printf("Policyholder's Smoking Status: %s\n", policy.getPolicyholderSmokingStatus());
+            System.out.printf("Policyholder's Height: %,.1f\n",      policy.getPolicyholderHeight());
+            System.out.printf("Policyholder's Weight: %,.1f\n",      policy.getPolicyholderWeight());
+            System.out.printf("Policyholder's BMI: %,.2f\n",         policy.calcBMI());
+            System.out.printf("Policy Price: $%,.2f\n",              policy.calcPolicyPrice());
+         }
+         System.out.println();
+         System.out.printf("The number of policies with a smoker is: %d\n", numSmoker);
+         System.out.printf("The number of policies with a non-smoker is: %d\n", policies.size() - numSmoker);
       } catch (FileNotFoundException e) {}
    }
 }
